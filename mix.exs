fe2048ba2d165a6cd968d216fca804fc76cee871
Kevin Castro <keacastroga@unal.edu.co>
2018-09-29 20:01:05 -0500

    Update .gitignore

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule PartidoService.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:corsica, "~> 1.0"}
+      {:corsica, "~> 1.0"},
     ]
   end
 
