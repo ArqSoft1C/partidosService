a2ad7e6119488f822e7eb074d9e703c64bba7906
Kevin Castro <keacastroga@unal.edu.co>
2018-09-29 22:17:42 -0500

    Modify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ do
   echo "$(date) - waiting for database to start"
   sleep 2
 done
-mix ecto.drop
+dropdb $PGDATABASE
 # Create, migrate, and seed database if it doesn't exist.
-if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
+if ! psql -U "$PGUSER" -lqtA | cut -d\| -f1 | grep -qFx "$PGDATABASE"; then
   echo "Database $PGDATABASE does not exist. Creating..."
   createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
   mix ecto.migrate
